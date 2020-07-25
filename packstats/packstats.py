4b088a45ba6237339fe232c9aa8a0158cbe6a470
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2020-07-25 20:47:11 +0530

    account for when packages are empty, identified by EMPTY_PACKAGE

--- packstats/packstats.py
+++ packstats/packstats.py
@@ -7,6 +7,7 @@ import os
 import urllib.request
 import pathlib
 
+from packstats.exceptions import ContentIndexForArchitectureNotFound
 
 def get_content_files_list(mirror_url: str = "http://ftp.uk.debian.org/debian/dists/stable/main/") -> list:
     """Returns a list of content files as defined
@@ -30,14 +31,17 @@ def get_content_files_list(mirror_url: str = "http://ftp.uk.debian.org/debian/di
     for line in html.split("\r\n"):
         if line.startswith("<a href=\"Contents-"):
             filename = line[line.find("Contents-"):line.find(".gz")+3]
-            url = f"{mirror_url}{filename}" if mirror_url.endswith("/") else f"{mirror_url}/{filename}"
+            url = f"{mirror_url}{filename}" if mirror_url.endswith(
+                "/") else f"{mirror_url}/{filename}"
             arch = filename[filename.find("-"):filename.rfind(".gz")-1]
             content_types.append(dict(filename=filename, url=url))
     return content_types
 
 
-def get_contents_file_urls(arch, mirror_url=None, include_udeb=False) -> list:
-    """Gets the URL(s) of the debian content index file for the given architecture"""
+def get_contents_file_urls(arch=None, mirror_url=None, include_udeb=False) -> list:
+    """Gets the URL(s) of the debian content index file for the given architecture
+    If the architecture is None it returns all the content indices.
+    """
     if mirror_url is not None:
         contents_file_list = get_content_files_list(mirror_url)
     else:
@@ -49,7 +53,7 @@ def get_contents_file_urls(arch, mirror_url=None, include_udeb=False) -> list:
         filename = file["filename"]
         file_arch = filename[filename.rfind("-")+1:filename.rfind(".gz")]
         is_udeb = filename.endswith(f"udeb-{file_arch}.gz")
-        if arch == file_arch:
+        if (arch is None) or (arch == file_arch):
             if is_udeb:
                 if include_udeb:
                     urls.append(url)
@@ -58,7 +62,7 @@ def get_contents_file_urls(arch, mirror_url=None, include_udeb=False) -> list:
     return urls
 
 
-def download_contents_file(content_file_url, output_dir=None, overwrite=True) -> str:
+def download_contents_file(content_file_url, output_dir=None, reuse_if_exists=False) -> str:
     """This function takes a Debian contents index and extracts the file to a given folder"""
     if output_dir is None:
         output_dir = os.getcwd()
@@ -70,7 +74,7 @@ def download_contents_file(content_file_url, output_dir=None, overwrite=True) ->
     # extracted contents index file path
     output_file = pathlib.Path(output_dir) / file_name
     if output_file.exists():
-        if not overwrite:
+        if reuse_if_exists:
             # TODO: implement a form of check where the filesize becomes a parameter of
             # interest.
             return output_file
@@ -103,57 +107,65 @@ def parse_contents_index(contents_index_file):
             continue
         else:
             file_name, packages = line[:line.rfind(
-                    " ")].strip(), line[line.rfind(" "):].strip()
+                " ")].strip(), line[line.rfind(" "):].strip()
             packages = packages.split(",")
             for package in packages:
                 if package_dict.get(package) is None:
-                    package_dict[package] = [file_name]
+                    package_dict[package] = [file_name] if file_name != "EMPTY_PACKAGE" else []
                 else:
                     package_dict[package].append(file_name)
     return package_dict
 
-def main(mirror_url, arch, count, include_udeb, sort_increasing):
 
-    # get one of these.
-    required = "amd64"
-    get_udeb = False
+def main(
+    mirror_url, arch, count, include_udeb, sort_increasing,
+    output_dir, reuse_if_exists, cleanup=False):
+    """This is the function that orchestrates the entirety of this application.
 
-    required_content_files = []
-    for content_file in content_types:
-        content_file_arch_matches = content_file.endswith(f"{required}.gz")
-        is_udeb = content_file.endswith(f"udeb-{required}.gz")
-        if is_udeb and get_udeb:
-            required_content_files.append(content_file)
-        elif content_file_arch_matches and not is_udeb:
-            required_content_files.append(content_file)
+    1. It gets a list of all content indices from a mirror url
+    2. It checks whether the provided arch is valid
+    3. It filters the content indices for the requested architecture
+    4. It downloads the content indices file(s)
+    5. It parses the content indices files(s)
+    6. It prints out the statistics"""
+    arch = arch.lower()  # use lowercase for the architecture.
+    # get a list of content index urls
+    content_indices_urls = get_contents_file_urls(
+        arch, mirror_url, include_udeb)
 
-    for file in required_content_files:
-        if not os.path.isfile(file):
-            with urllib.request.urlopen(f"{mirror_url}{file}") as response:
-                data = response.read()
-                with open(file, "wb") as f:
-                    f.write(data)
-                print(file)
+    if len(content_indices_urls) == 0:
+        # if the architecture didn't retrieve anything, output an error,
+        # with a list of architectures
+        content_indices_urls = get_contents_file_urls()
+        found_architectures = [item["arch"] for item in content_indices_urls]
+        found_architectures = ", ".join(found_architectures)
+        # raise a custom exception
+        raise ContentIndexForArchitectureNotFound(
+            f"{arch} was not found in the given mirror. Available architectures are: {found_architectures}")
 
-    file_data_dict = {}
-    for file in required_content_files:
-        # unzip them
-        with gzip.open(file, "rb") as f:
-            data = f.read()
-            data = data.decode()
-            file_data_dict[file] = {}
-            for ix, row in enumerate(data.split("\n")):
-                if row.strip() == "":
-                    continue
-                file_name, packages = row[:row.rfind(
-                    " ")].strip(), row[row.rfind(" "):].strip()
-                packages = packages.split(",")
-                for package in packages:
-                    if file_data_dict[file].get(package) is None:
-                        file_data_dict[file][package] = [file_name] if filename != "EMPTY_PACKAGE" else []
-                    else:
-                        if file_name != "EMPTY_PACKAGE":
-                            file_data_dict[file][package].append(file_name)
+    # prepare to summarize the data
+    complete_package_data = {}
+    for url in content_indices_urls:
+        content_index_file = download_contents_file(url, output_dir=output_dir, reuse_if_exists=reuse_if_exists)
+        package_data = parse_contents_index(content_index_file)
+        complete_package_data.update(**package_data)
+
+    package_list = complete_package_data.keys()
+    # sort them in the ascending order of the number of packages
+    package_list = sorted(
+        package_list, key=lambda x: len(complete_package_data[x]))
+    # reverse the sorting direction unless the user wants the ascending order
+    if not sort_increasing:
+        package_list = reversed(package_list)
+
+    # print the output, only print how many ever the user asks through the count argument
+
+    for ix, package in enumerate(package_list):
+        if ix == 0:
+            print(f"No.\t{'Package Name':<50}\tFile Count")
+        print(f"{ix+1}.\t{package:<50}\t{len(complete_package_data[package])}")
+        if ix+1 == count:
+            break # use this instead of slicing the reversed list because this way, the overhead of converting a giant iterator to a list is avoided. Some milliseconds may be saved.
 
 
 def cli_main():
@@ -161,7 +173,7 @@ def cli_main():
     parser = argparse.ArgumentParser(
         description=(
             "A tool to get the package statistics by parsing "
-            "a Contents index (defined here - https://wiki.debian.org/RepositoryFormat#A.22Contents.22_indices)"
+            "a Contents Index (defined here - https://wiki.debian.org/RepositoryFormat#A.22Contents.22_indices)"
             "from a debian mirror, given a system architecture."
         )
     )
@@ -182,17 +194,22 @@ def cli_main():
     parser.add_argument("-c", "--count", type=int, default=10,
                         help="number of packages to list. DEFAULT 10")
     parser.add_argument(
-        "-d", "--sort-increasing", action="store_true",
+        "-i", "--sort-increasing", action="store_true",
         help="Sort package stats list by increasing number of files. DEFAULT False")
+    parser.add_argument(
+        "-o", "--output-dir", type=str, default=os.getcwd(),
+        help="a directory in which to store the downloaded contents indices. DEFAULT <current-directory>")
+    parser.add_argument(
+        "-r", "--reuse-if-exists", help="Reuses a content file if it has been downloaded previously and exists in the output directory.",
+        action="store_true",
+    )
     args = parser.parse_args()
     main(
-        mirror=args.mirror_url,
+        mirror_url=args.mirror_url,
         arch=args.arch,
         include_udeb=args.include_udeb,
         sort_increasing=args.sort_increasing,
         count=args.count,
+        output_dir=args.output_dir,
+        reuse_if_exists=args.reuse_if_exists,
     )
-
-
-if __name__ == "__main__":
-    cli_main()
