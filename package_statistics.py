fe07ab708d3a620e6ced791287e90313ac513d92
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2020-07-25 20:48:38 +0530

    add helper file if users don't want to install this package

---
+++ package_statistics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This is a helper runner file provided if you don't want
+to install the packstats package.
+
+Check the README.md for more information.
+
+"""
+
+from packstats import cli_main
+
+
+if __name__ == "__main__":
+    cli_main()
