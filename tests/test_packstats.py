ad44c1f2466e706d716643ce9695ef1743dbf4e3
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2020-07-25 19:18:11 +0530

    add test to check that the application can parse the contents index file correctly

--- tests/test_packstats.py
+++ tests/test_packstats.py
@@ -5,7 +5,7 @@ import pathlib
 class TestPackTest(unittest.TestCase):
     """This test case tests the entirety of packstats"""
     def setUp(self) -> None:
-        self.architecture = "amd64"
+        self.architecture = "armel"
 
     def test_lists_architectures(self):
         """the application can list the architectures in the provided debian mirror"""
@@ -47,7 +47,7 @@ class TestPackTest(unittest.TestCase):
         """the application can download the right contents file given an architecture
         string"""
         from packstats.packstats import download_contents_file, get_contents_file_urls
-        urls = get_contents_file_urls("amd64")
+        urls = get_contents_file_urls(self.architecture)
 
         with tempfile.TemporaryDirectory() as temp_directory:
             url = urls[0]
@@ -59,7 +59,24 @@ class TestPackTest(unittest.TestCase):
 
 
     def test_gets_package_data_from_contents(self):
-        """the application can get the package data from a contents file"""
+        """the application can get the package data from a contents file
+
+        Also checks that that when the udeb files are added, the package gets
+        extended."""
+        from packstats.packstats import get_contents_file_urls, download_contents_file, parse_contents_index
+        urls = get_contents_file_urls(self.architecture, include_udeb=True)
+        with tempfile.TemporaryDirectory() as temp_directory:
+            # merge udeb and deb info, if required.
+            complete_package_data = {}
+            total_keys = 0
+            for url in urls:
+                contents_file = download_contents_file(url)
+                package_dict = parse_contents_index(contents_file)
+                self.assertIsInstance(package_dict, dict, "Package metadata should be a dictionary")
+                self.assertTrue(len(package_dict.keys()) > 0, "Package list is not empty")
+                complete_package_data.update(**package_dict)
+                total_keys += len(package_dict)
+            self.assertEqual(total_keys, len(complete_package_data), "the two package files were not added together")
 
     def test_prints_top_ten_packages_given_arch(self):
         """The application can output the top ten packages given the architecture"""
