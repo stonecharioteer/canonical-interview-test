96b3eaf3c112b19960522c8c0d18bfb345721b94
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2020-07-26 11:29:07 +0530

    add module docstring

--- packstats/__main__.py
+++ packstats/__main__.py
@@ -1,3 +1,10 @@
+"""This module enables usage such as
+python -m packstats
+
+This is needed for standard behaviour compliance
+with packages such as pip and other builtins.
+"""
+
 from .packstats import cli_main
 
 
